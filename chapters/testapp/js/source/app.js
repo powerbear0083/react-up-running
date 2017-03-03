cfdc2fd23d62876dc9a9e2ffb1d147b4e11b489b
eu-company <powerbear008@gmail.com>
2017-03-03 17:48:02 +0800

    convert erb to pug

---
+++ chapters/testapp/js/source/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Logo from './components/Logo.js';
+
+
+ReactDOM.render(
+	<h1>
+		<Logo /> Welcome to the app!
+	</h1>,
+	document.getElementById('app');
+);
